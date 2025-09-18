f22d5bb90c15d754641612584d7380884e1eaff4
radius-roundabout <206484938+radius-roundabout@users.noreply.github.com>
2025-09-17 21:49:15 -0400

    All methods work.

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -8,13 +8,65 @@ module Enumerable
 
   def my_select
     results = []
-    self.each do |element|
+    self.my_each do |element|
       if yield(element)
         results.push(element)
       end
     end
     results
   end
+
+  def my_all?
+    self.my_each do |element|
+      return false unless yield(element)
+    end
+    true 
+  end
+
+  def my_any?
+    self.my_each do |element|
+      return true if yield(element)
+    end
+    false 
+  end
+
+  def my_none?
+    self.my_each do |element|
+      return false if yield(element)
+    end
+    true
+  end
+
+  def my_count
+    count = 0
+    return self.length unless block_given?
+    self.my_each do |element|
+      count += 1 if yield(element)
+    end
+    count
+  end
+
+  def my_map(&block)
+    results = []
+    self.my_each do |element|
+      result = block.call(element)
+      results.push(result)
+    end
+    results
+  end
+
+  def my_inject(initial_value = 0)
+    result = initial_value
+    self.my_each do |element|
+      if result == 0 
+        result = element
+        next 
+      end
+      result = yield(result, element)
+    end
+    result
+  end
+
 end
 
 # You will first have to define my_each
@@ -27,5 +79,6 @@ class Array
     self.each do |elem|
       yield(elem)
     end
+    self
   end
 end
