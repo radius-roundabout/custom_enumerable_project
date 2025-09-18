b6c3fa1c5e0253cc485caf59856243c4682caf49
radius-roundabout <206484938+radius-roundabout@users.noreply.github.com>
2025-09-17 20:17:57 -0400

    Adds the first few enumerables.

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,5 +1,20 @@
 module Enumerable
   # Your code goes here
+  def my_each_with_index
+    self.each_with_index do |element, index|
+      yield(element, index)
+    end
+  end
+
+  def my_select
+    results = []
+    self.each do |element|
+      if yield(element)
+        results.push(element)
+      end
+    end
+    results
+  end
 end
 
 # You will first have to define my_each
@@ -8,4 +23,9 @@ end
 # to this method
 class Array
   # Define my_each here
+  def my_each
+    self.each do |elem|
+      yield(elem)
+    end
+  end
 end
